61cd0e8d07e34020a31dedf69e51d2fabc3acb28
keir <darksalterego99@gmail.com>
2025-12-05 08:51:31 +0530

    added dependencies and implemented container runtime functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use nix::sched::{clone, CloneFlags};
+use nix::sys::wait::waitpid;
+use nix::unistd::{execvp};
+use std::ffi::CString;
+
+
+const STACK_SIZE: usize = 1024 * 1024; // it will be a 1mb stack for the child process
+
 fn main() {
-    println!("Hello, world!");
+    println!("[Parent] Aegis Container Runtime is starting ...");
+
+    let mut stack = vec![0; STACK_SIZE];
+    let flags = CloneFlags::CLONE_NEWPID;
+
+    let pid = unsafe {
+        clone (
+            Box::new(|| child_process()),
+            &mut stack,
+            flags,
+            Some(nix::sys::signal::Signal::SIGCHLD as i32)
+        )
+    };
+
+    match pid {
+        Ok(child_pid) => {
+            println!("[Parent] Created container with PID: {}", child_pid);
+
+            let _ = waitpid(child_pid, None);
+            println!("[Parent] Container with PID: {} has exited", child_pid);
+            
+        }
+        Err(e) => eprintln!("[Parent] Failed to create container: {}", e),
+
+    }
+}
+
+fn child_process() -> isize {
+    println!("[Child] I'm alive! preparing for brain transplant");
+
+    let command = CString::new("sh").expect("CString Failed");
+    let args = [command.clone()];
+
+    println!("[Child] Executing /bin/sh....");
+
+    match execvp(&command, &args) {
+        Ok(_) => 0,
+        Err(e) => {
+            eprintln!("[Child] execvp failed: {}", e);
+            -1
+        }
+    }
 }
